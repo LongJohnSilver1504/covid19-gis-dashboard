6fbc1d16140693eacf332e391227cba1fd11ea08
John Silver <johnsilver1504@zoho.com>
2020-06-02 23:57:07 -0400

    feat: Add component page o header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
 
-import Container from 'components/Container';
+import Container from "components/Container";
 
 const Header = () => {
   return (
@@ -13,7 +13,7 @@ const Header = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/page-2/">Page 2</Link>
+            <Link to="/components">Components</Link>
           </li>
         </ul>
       </Container>
