41efd26331c81ca550c9a7f71ea85b37852e62aa
John Silver <johnsilver1504@zoho.com>
2020-06-06 20:42:04 -0400

    build:Add prettier and eslint package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 module.exports = {
   plugins: [
     "gatsby-plugin-material-ui",
@@ -12,5 +13,28 @@ module.exports = {
       },
     },
     "gatsby-plugin-react-leaflet",
+    {
+      resolve: "gatsby-plugin-prettier-eslint",
+      options: {
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            "**/*.{css,scss,less}",
+            "**/*.{json,json5}",
+            "**/*.{graphql}",
+            "**/*.{md,mdx}",
+            "**/*.{html}",
+            "**/*.{yaml,yml}",
+          ],
+        },
+        eslint: {
+          patterns: "**/*.{js,jsx,ts,tsx}",
+          customOptions: {
+            fix: true,
+            cache: true,
+          },
+        },
+      },
+    },
   ],
 };
