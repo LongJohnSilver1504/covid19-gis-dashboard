9611cd7dc190695fbf7e0b1d1b10616e12349c89
John Silver <johnsilver1504@zoho.com>
2020-06-07 13:01:33 -0400

    refactor: Remove unused color

--- src/components/global/AppWrapper.js
+++ src/components/global/AppWrapper.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
-import { orange } from "@material-ui/core/colors";
 import theme from "../../global/Theme/theme";
 
 const AppWrapper = ({ children }) => (
   <ThemeProvider theme={theme}>{children}</ThemeProvider>
 );
 
+// eslint-disable-next-line react/display-name
 export default ({ element }) => <AppWrapper>{element}</AppWrapper>;
