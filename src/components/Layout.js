ddf218cb166ebd4775c03fa99e75a2fb48840ba6
John Silver <johnsilver1504@zoho.com>
2020-06-07 20:07:12 -0400

    feat: Update Layout template

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,29 +1,90 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 
-import 'assets/stylesheets/application.scss';
+import "assets/stylesheets/application.scss";
+import { makeStyles, Grid } from "@material-ui/core";
+import SideBar from "./SideBar";
 
-import Header from 'components/Header';
-import Footer from 'components/Footer';
+const useStyles = makeStyles({
+  container: {
+    margin: 20,
+    height: 600,
+  },
+  content: {
+    background: "#ffffff",
+    borderRadius: 30,
+    boxShadow: "25px 25px 50px 0 rgba(0, 0, 0, 0.16)",
+    maxWidth: 1440,
 
-const Layout = ({ children, pageName }) => {
-  let className = '';
+    position: "absolute",
+    left: "50%",
+    top: "50%",
 
-  if ( pageName ) {
-    className = `${className} page-${pageName}`;
-  }
+    transform: "translate(-50%, -50%)",
+  },
+  iconSection: {},
+  secondarySection: {
+    background: "#c2cde1",
+    borderRadius: "30px  ",
+  },
+  mainSection: {
+    background: "#f5f5f5",
+    borderRadius: "30px ",
+  },
+});
 
+const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
     <>
-      <Helmet bodyAttributes={{ class: className }}>
+      <Helmet>
         <title>Gatsby Site</title>
       </Helmet>
-      <div className="wrapper">
-        <Header />
-        <main>{ children }</main>
-        <Footer />
-      </div>
+      <Grid
+        className={classes.container}
+        direction="row"
+        justify="center"
+        alignItems="center"
+        container
+      >
+        <Grid container item xs={12} className={classes.content}>
+          <Grid
+            className={classes.iconSection}
+            item
+            xs={2}
+            md={1}
+            lg={1}
+            xl={1}
+            container
+          >
+            <SideBar />
+          </Grid>
+          <Grid
+            container
+            className={classes.secondarySection}
+            item
+            xs={10}
+            md={11}
+            lg={11}
+            xl={11}
+          >
+            <Grid xs={4} md={2} lg={2} xl={2} item>
+              <p>Secondary</p>
+            </Grid>
+            <Grid
+              className={classes.mainSection}
+              xs={8}
+              md={10}
+              lg={10}
+              xl={10}
+              item
+            >
+              <main style={{ height: 600, overflow: "auto" }}>{children}</main>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 };
