c808c3e0c33dd96592cfa30329e8750ff3640cdd
John Silver <johnsilver1504@zoho.com>
2020-06-07 20:06:08 -0400

    feat: Create Gis page template

---
+++ src/pages/gis.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import Layout from "components/Layout";
+import Container from "components/Container";
+import Typography from "@material-ui/core/Typography";
+
+const GisPage = () => {
+  return (
+    <Layout pageName="Gid">
+      <Helmet>
+        <title>Gis</title>
+      </Helmet>
+      <Container type="content">
+        <Typography variant="h1">Gis</Typography>
+      </Container>
+    </Layout>
+  );
+};
+
+export default GisPage;
