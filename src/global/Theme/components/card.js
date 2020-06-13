2ef6ba2eac0dbf1603a682a3388fc1c4e1ddb07e
John Silver <johnsilver1504@zoho.com>
2020-06-13 19:13:25 -0400

    style: Customize material card component

---
+++ src/global/Theme/components/card.js
@@ -0,0 +1,7 @@
+export const MuiCard = {
+  root: {
+    borderRadius: 20,
+    padding: 26,
+    boxShadow: " 0 3px 40px 0 rgba(0, 0, 0, 0.08)",
+  },
+};
--- src/global/Theme/theme.js
+++ src/global/Theme/theme.js
@@ -2,12 +2,14 @@ import { createMuiTheme } from "@material-ui/core/styles";
 import { MuiButton } from "./components/button";
 import { typography } from "./components/typography";
 import { palette } from "./palette";
+import { MuiCard } from "./components/card";
 
 const theme = createMuiTheme({
   palette,
   typography,
   overrides: {
     MuiButton,
+    MuiCard,
   },
 });
 
